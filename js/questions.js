ab866e03cd781a5edbdd4634e539e1febbf494f1
tyhaggis86 <113810488+tyhaggis86@users.noreply.github.com>
2022-11-24 14:55:07 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of Santa's reindeer has the same name as another holiday mascot??",
+    answer: "Cupid",
+    options: [
+      "Prancer",
+      "Cupid",
+      "Comet",
+      "Donner"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many ghosts show up in A Christmas Carol?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The movie Miracle on 34th Street is based on a real-life department store?",
+    answer: "Macys",
+    options: [
+      "Macys",
+      "Bloomingdales",
+      "Saks Fifth Avenue"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In Home Alone, where are the McCallisters going on vacation when they leave Kevin behind?",
+    answer: "Paris",
+    options: [
+      "Rome",
+      "New York",
+      "Paris",
+      "London"
+    ]
+  },
+  //   {
+  //   numb: 5,
+  //   question: "What does XML stand for?",
+  //   answer: "eXtensible Markup Language",
+  //   options: [
+  //     "eXtensible Markup Language",
+  //     "eXecutable Multiple Language",
+  //     "eXTra Multi-Program Language",
+  //     "eXamine Multiple Language"
+  //   ]
+  // },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
